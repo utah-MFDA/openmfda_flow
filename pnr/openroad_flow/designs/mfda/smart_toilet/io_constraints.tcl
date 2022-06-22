8af6358ad6c858a4779086401bb487a9ebe666e6
Daniel Wakeham <danielwakeham@Daniels-MacBook-Pro.local>
2022-06-22 12:53:15 -0600

    new daniel_dev branch

--- pnr/openroad_flow/designs/mfda/smart_toilet/io_constraints.tcl
+++ pnr/openroad_flow/designs/mfda/smart_toilet/io_constraints.tcl
@@ -3,5 +3,5 @@
                         -pin_names <names> \
                         -region <edge:interval>
 
-place_pin -pin_name in_fluid0 -layer met1 -location {350 950} -pin_size {1 1}
-place_pin -pin_name in_fluid1 -layer met1 -location {1350 950} -pin_size {1 1}
+place_pin -pin_name a_fluid -layer met1 -location {1100 800} -pin_size {1 1}
+place_pin -pin_name b_fluid -layer met1 -location {1450 800} -pin_size {1 1}
