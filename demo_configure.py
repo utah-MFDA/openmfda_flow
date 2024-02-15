d005af19095105cbf58a6929c070681a1191b9a9
Ashton Snelgrove <ashton@snelgrove.science>
2024-02-14 23:01:40 -0700

    Commit demo configuration script, and add its outputs to the gitignore.

---
+++ demo_configure.py
@@ -0,0 +1,13 @@
+from openmfda_flow import *
+
+verilog_file = "demo.v"
+design_name = "demo"
+platform = "mfda_30px"
+pins = [[None for i in range(0,8)] for j in range(0,4)]
+pins[0][1] = "soln1"
+pins[0][2] = "soln2"
+pins[0][3] = "soln3"
+pins[1][7] = "out"
+
+generate_config(verilog_file, design_name, pin_names=pins, platform=platform)
+run_flow(design_name, platform=platform)
