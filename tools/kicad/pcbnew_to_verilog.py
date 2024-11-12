c5a27f2aa8714574d92948e77f62b00ed984fcf6
Ashton Snelgrove <ashton@snelgrove.science>
2024-11-12 12:54:29 -0700

    Get the correct field from pcbnew for the footprint id in pcbnew-to-verilog

--- tools/kicad/pcbnew_to_verilog.py
+++ tools/kicad/pcbnew_to_verilog.py
@@ -1,30 +1,27 @@
 import itertools
 import math
 from kiutils.schematic import Schematic
-import networkx as nx
 import pcbnew
 
 class PcbToVerilog:
     def __init__(self, board, design):
         self.design = design
         self.board = board
-        self.graph = nx.Graph()
-
 
     def print_verilog(self, outfile):
-        print(f"module {design} ();"), file=outfile)
+        print(f"module {design} ();", file=outfile)
         for net, item in self.board.GetNetsByName().items():
             if len(str(net)):
                 print(f"\twire \\{net} ;") # Space after the name is important here!, file=outfile)
         for foot in self.board.GetFootprints():
-            module = foot.GetValue()
+            module = foot.GetFPID().GetLibItemName().utf8_to_wxstring()
             name = foot.GetReference()
             # Space after the name is important here!
-            print(f"\t\\{module} \\{name} ("), file=outfile)
+            print(f"\t\\{module} \\{name} (", file=outfile)
             # Space after the name is important here!
-            print(*[f"\t\t.\\{pad.GetName()} (\\{pad.GetNetname()} )" for pad in foot.Pads() if len(str(pad.GetNetname()))], sep=",\n"), file=outfile)
-            print("\t);"), file=outfile)
-        print("endmodule"), file=outfile)
+            print(*[f"\t\t.\\{pad.GetName()} (\\{pad.GetNetname()} )" for pad in foot.Pads() if len(str(pad.GetNetname()))], sep=",\n", file=outfile)
+            print("\t);", file=outfile)
+        print("endmodule", file=outfile)
 
 if __name__ == "__main__":
     import sys
