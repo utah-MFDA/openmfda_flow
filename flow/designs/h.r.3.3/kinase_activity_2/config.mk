41c0497f85f383a60d64a61ea999dbbe2c91f7b7
Ashton Snelgrove <ashton@snelgrove.science>
2025-02-10 10:28:57 -0700

    Update kinase_activity_2 to use new openroad syntax and switch from bump pad to pin. Bump pads generate but special nets are not routed.

--- flow/designs/h.r.3.3/kinase_activity_2/config.mk
+++ flow/designs/h.r.3.3/kinase_activity_2/config.mk
@@ -2,6 +2,7 @@ export DESIGN_NAME     	= kinase_activity_2
 
 export VERILOG_FILES 	= ./designs/src/$(DESIGN_NICKNAME)/kinase_activity_2.v ./designs/src/$(DESIGN_NICKNAME)/kinase_activity.v ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/pad_wrapper.v
 export SDC_FILE      	= ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+export IO_CONSTRAINTS	= ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/pins.tcl
 export FOOTPRINT_TCL = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/pads.tcl
 export MACRO_PLACE_TCL = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/macro.tcl
 export DIE_AREA    	 	= 0 0 2560 1600
--- flow/designs/h.r.3.3/kinase_activity_2/pad_wrapper.v
+++ flow/designs/h.r.3.3/kinase_activity_2/pad_wrapper.v
@@ -6,20 +6,20 @@ module kinase_activity_2(input in1_a, in2_a, in3_a,
                                  p1, p2, p3, p4, p5,
                                  output out1_a, out2_a, out3_a, out4_a,
                                  out1_b, out2_b, out3_b, out4_b);
-  pinhole_325px_met3 pinhole1(.pad(in1_a), .connection(in1_a_X));
-  pinhole_325px_met3 pinhole2(.pad(in2_a), .connection(in2_a_X));
-  pinhole_325px_met3 pinhole3(.pad(in3_a), .connection(in3_a_X));
-  pinhole_325px_met3 pinhole4(.pad(in1_b), .connection(in1_b_X));
-  pinhole_325px_met3 pinhole5(.pad(in2_b), .connection(in2_b_X));
-  pinhole_325px_met3 pinhole6(.pad(in3_b), .connection(in3_b_X));
-  pinhole_325px_met3 pinhole7(.connection(out1_a_X), .pad(out1_a));
-  pinhole_325px_met3 pinhole8(.connection(out2_a_X), .pad(out2_a));
-  pinhole_325px_met3 pinhole9(.connection(out3_a_X), .pad(out3_a));
-  pinhole_325px_met3 pinhole10(.connection(out4_a_X), .pad(out4_a));
-  pinhole_325px_met3 pinhole11(.connection(out1_b_X), .pad(out1_b));
-  pinhole_325px_met3 pinhole12(.connection(out2_b_X), .pad(out2_b));
-  pinhole_325px_met3 pinhole13(.connection(out3_b_X), .pad(out3_b));
-  pinhole_325px_met3 pinhole14(.connection(out4_b_X), .pad(out4_b));
+  pinhole_325px_0 pinhole1(.pad(in1_a), .connection(in1_a_X));
+  pinhole_325px_0 pinhole2(.pad(in2_a), .connection(in2_a_X));
+  pinhole_325px_0 pinhole3(.pad(in3_a), .connection(in3_a_X));
+  pinhole_325px_0 pinhole4(.pad(in1_b), .connection(in1_b_X));
+  pinhole_325px_0 pinhole5(.pad(in2_b), .connection(in2_b_X));
+  pinhole_325px_0 pinhole6(.pad(in3_b), .connection(in3_b_X));
+  pinhole_325px_0 pinhole7(.connection(out1_a_X), .pad(out1_a));
+  pinhole_325px_0 pinhole8(.connection(out2_a_X), .pad(out2_a));
+  pinhole_325px_0 pinhole9(.connection(out3_a_X), .pad(out3_a));
+  pinhole_325px_0 pinhole10(.connection(out4_a_X), .pad(out4_a));
+  pinhole_325px_0 pinhole11(.connection(out1_b_X), .pad(out1_b));
+  pinhole_325px_0 pinhole12(.connection(out2_b_X), .pad(out2_b));
+  pinhole_325px_0 pinhole13(.connection(out3_b_X), .pad(out3_b));
+  pinhole_325px_0 pinhole14(.connection(out4_b_X), .pad(out4_b));
 
   interconnect_8x4 ic(.pin_0_0(c1_X), .pin_0_0_pad(c1),
                       .pin_0_1(c9_X), .pin_0_1_pad(c9),
