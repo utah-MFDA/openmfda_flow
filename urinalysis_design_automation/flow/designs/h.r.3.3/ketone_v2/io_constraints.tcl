3eaebed6ebc57b9705b318a821dac960f911b1e7
Erica Francis <ferica963@gmail.com>
2025-09-04 12:20:46 -0600

    Files to run design automation of urinalysis suite

--- urinalysis_design_automation/flow/designs/h.r.3.3/ketone_v2/io_constraints.tcl
+++
@@ -1,3 +0,0 @@
-place_pin -pin_name soln1 -layer met9 -location { 1050 660 } -pin_size {1 1}
-place_pin -pin_name soln2 -layer met9 -location { 1140 660 } -pin_size {1 1}
-place_pin -pin_name out -layer met9 -location { 1590 750 } -pin_size {1 1}
