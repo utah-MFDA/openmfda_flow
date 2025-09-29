56d7e42f2d23fe612acb4e8405e6bc7f52f11569
Ashton Snelgrove <ashton@snelgrove.science>
2025-09-29 13:50:05 -0600

    Working hypergraph reader

--- tools/3d_route/hypernetx_helpers.py
+++ tools/3d_route/hypernetx_helpers.py
@@ -4,52 +4,54 @@ import hypernetx as hnx
 import json
 import sys
 
-def hypergraph_to_graph(H: hnx.Hypergraph):
-    G = nx.Graph()
-    for node in H.nodes:
-        G.add_node(node, hyperedge=False, **H.nodes[node])
-    for edge in H.edges:
-        if len(H.incidences[edge].nodes) == 2:
-            s, e = H.incidences[edge].nodes
-            G.add_edge(s,e, **H.edges[edge], hyperedge=False)
-        else:
-            G.add_node(edge, hyperedge=True, **H.edges[edge])
-            for node in H.incidences[edge].nodes:
-                G.add_edge(node, edge, hyperedge=True, **H.incidences[edge][node])
-    return G
+# def hypergraph_to_graph(H: hnx.Hypergraph):
+#     G = nx.Graph()
+#     for node in H.nodes:
+#         G.add_node(node, hyperedge=False, **H.nodes[node])
+#     for edge in H.edges:
+#         if len(H.incidences[edge].nodes) == 2:
+#             s, e = H.incidences[edge].nodes
+#             G.add_edge(s,e, **H.edges[edge], hyperedge=False)
+#         else:
+#             G.add_node(edge, hyperedge=True, **H.edges[edge])
+#             for node in H.incidences[edge].nodes:
+#                 G.add_edge(node, edge, hyperedge=True, **H.incidences[edge][node])
+#     return G
 
 def read_yosys_json_hyper(input_file, top):
-    H = hnx.Hypergraph()
-
+    G = hnx.Hypergraph()
     with open(input_file) as f:
         y = json.load(f)
 
     m = y["modules"][top]
-    for io, detail in m["ports"].items():
-        H.add_node(io, kind="port", direction=detail["direction"])
-        for i in detail["bits"]:
-            H.add_incidence(str(i), io)
 
     for cell, detail in m["cells"].items():
-        H.add_node(cell, weight=1, kind=detail["type"],
+        if detail["type"] == "$scopeinfo":
+            continue
+        G.add_node(cell, cell=detail["type"],
                    parameters=detail["parameters"], attributes=detail["attributes"])
-        print("cell", cell, H.nodes[cell]["kind"])
-
         for port, bits in detail["connections"].items():
             if "port_directions" in detail:
                 direction = detail["port_directions"][port]
             else:
                 direction = "unknown"
             for bit in bits:
-                H.add_incidence(cell, bit, port=port, direction=direction)
-    # print(*(h for h in H.edges), sep="\n")
-    # print("***")
-    # print(*(h for h in H.nodes), sep="\n")
+                bit = f"$_{bit}"
+                G.add_node(bit, weight=0, cell="$_wire", netnames=set())
+                G.add_incidence( bit,cell, port=port, direction=direction)
     for net, details in m["netnames"].items():
+        # Multiple named nets will be attached to the same bit, last one wins.
         for index, bit in enumerate(details["bits"]):
-            if "hide_name" in details:
-                H.edges[bit]["hide_name"] = details["hide_name"]
-            H.edges[bit]["netname"] = net
-            H.edges[bit]["index"] = index
-            H.edges[bit]["attributes"] = details["attributes"]
-    return H
+            bit = f"$_{bit}"
+            if not details.get("hide_name", 0):
+                G.get_properties(bit, 1)["netnames"].add((net, details.get("index", 0)))
+                # there is a possible offset, ignoring
+            G.get_properties(bit, 1)["attributes"] = details["attributes"]
+            G.get_properties(bit, 1)["test"] = index
+            if "type" in details["attributes"]:
+                G.get_properties(bit, 1)["type"] = details["attributes"]["type"]
+            # If the wire type is input/output/inout, mark here
+            if net in m["ports"]:
+                direction = m["ports"][net]["direction"]
+                G.get_properties(bit, 1)["cell"] = f"$_{direction}"
+    return G
