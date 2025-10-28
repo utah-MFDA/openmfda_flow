0128b6ac6c5b1e76a619036a13a4fbc7e5f7eee8
Ashton Snelgrove <ashton@snelgrove.science>
2025-10-28 11:39:13 -0600

    Plugin layout cleanup

--- tools/kicad/server_plugins.py
+++ tools/kicad/server_plugins.py
@@ -13,7 +13,7 @@ class RequestDialog(wx.Dialog):
     """
 
     def __init__(self, filename, route, suffix, *args, **kw):
-        super(RequestDialog, self).__init__(style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.MAXIMIZE_BOX, *args, **kw)
+        super(RequestDialog, self).__init__(style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.MAXIMIZE, *args, **kw)
         panel = wx.Panel(self)
         self.log = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_WORDWRAP | wx.TE_READONLY)
         self.hostname= wx.TextCtrl(panel)
@@ -29,7 +29,7 @@ class RequestDialog(wx.Dialog):
 
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.log, 1, wx.ALL|wx.EXPAND, 5)
-        sizer.Add(self.hostname, 0, wx.ALL, 5)
+        sizer.Add(self.hostname, 0, wx.ALL|wx.EXPAND, 5)
         sizer.Add(start_btn, 0, wx.ALL, 5)
         sizer.Add(halt_btn, 0, wx.ALL, 5)
         panel.SetSizer(sizer)
