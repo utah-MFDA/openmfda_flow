ef1486d3ffc75153191b174c57c957677c0cd461
Erica Francis <ferica963@gmail.com>
2026-01-14 11:34:52 -0700

    Suquential multiplex Verilog file

---
+++ flow/designs/h.r.3.3/multiplex/io_constraints.tcl
@@ -0,0 +1,45 @@
+# FUNCTIONALITY
+# set_io_pin_constraint   -direction <direction>      \
+#                         -pin_names <names>          \
+#                         -region <edge:interval>
+#
+# place_pin   -pin_name <pin_name>        \
+#             -layer <layer>              \
+#             -location <{x y}>           \
+#             -pin_size <{width height}>
+
+place_pin -pin_name soln1 -layer met9 -location {960 930} -pin_size {1 1}
+place_pin -pin_name soln2 -layer met9 -location {1050 930} -pin_size {1 1}
+place_pin -pin_name soln3 -layer met9 -location {1140 930} -pin_size {1 1}
+place_pin -pin_name soln4 -layer met9 -location {1230 930} -pin_size {1 1}
+place_pin -pin_name soln5 -layer met9 -location {1320 930} -pin_size {1 1}
+place_pin -pin_name soln6 -layer met9 -location {1410 930} -pin_size {1 1}
+place_pin -pin_name soln7 -layer met9 -location {1500 930} -pin_size {1 1}
+place_pin -pin_name soln8 -layer met9 -location {1590 930} -pin_size {1 1}
+
+place_pin -pin_name soln9 -layer met9 -location {960 840} -pin_size {1 1}
+place_pin -pin_name soln10 -layer met9 -location {1050 840} -pin_size {1 1}
+place_pin -pin_name soln11 -layer met9 -location {1140 840} -pin_size {1 1}
+place_pin -pin_name soln12 -layer met9 -location {1230 840} -pin_size {1 1}
+#place_pin -pin_name soln13 -layer met9 -location {1320 840} -pin_size {1 1}
+#place_pin -pin_name soln14 -layer met9 -location {1410 840} -pin_size {1 1}
+#place_pin -pin_name soln15 -layer met9 -location {1500 840} -pin_size {1 1}
+#place_pin -pin_name soln16 -layer met9 -location {1590 840} -pin_size {1 1}
+
+#place_pin -pin_name soln17 -layer met9 -location {960 750} -pin_size {1 1}
+#place_pin -pin_name soln18 -layer met9 -location {1050 750} -pin_size {1 1}
+#place_pin -pin_name pin_33 -layer met9 -location {1140 750} -pin_size {1 1}
+#place_pin -pin_name pin_34 -layer met9 -location {1230 750} -pin_size {1 1}
+#place_pin -pin_name pin_35 -layer met9 -location {1320 750} -pin_size {1 1}
+#place_pin -pin_name pin_36 -layer met9 -location {1410 750} -pin_size {1 1}
+#place_pin -pin_name pin_37 -layer met9 -location {1500 750} -pin_size {1 1}
+#place_pin -pin_name pin_38 -layer met9 -location {1590 750} -pin_size {1 1}
+
+place_pin -pin_name out -layer met9 -location {960 660} -pin_size {1 1}
+place_pin -pin_name out2 -layer met9 -location {1050 660} -pin_size {1 1}
+place_pin -pin_name out3 -layer met9 -location {1140 660} -pin_size {1 1}
+place_pin -pin_name out4 -layer met9 -location {1230 660} -pin_size {1 1}
+place_pin -pin_name out5 -layer met9 -location {1320 660} -pin_size {1 1}
+#place_pin -pin_name out6 -layer met9 -location {1410 660} -pin_size {1 1}
+#place_pin -pin_name out7 -layer met9 -location {1500 660} -pin_size {1 1}
+#place_pin -pin_name out8 -layer met9 -location {1590 660} -pin_size {1 1}
