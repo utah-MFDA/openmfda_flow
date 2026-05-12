57853473d49eff297940fea9a7430b7d0ed8f648
Erica Francis <ferica963@gmail.com>
2026-05-12 13:21:55 -0600

    Urinalysis multiplex device

--- flow/designs/h.r.3.3/multiplex/io_constraints.tcl
+++ flow/designs/h.r.3.3/multiplex/io_constraints.tcl
@@ -21,7 +21,7 @@ place_pin -pin_name soln9 -layer met9 -location {960 840} -pin_size {1 1}
 place_pin -pin_name soln10 -layer met9 -location {1050 840} -pin_size {1 1}
 place_pin -pin_name soln11 -layer met9 -location {1140 840} -pin_size {1 1}
 place_pin -pin_name soln12 -layer met9 -location {1230 840} -pin_size {1 1}
-#place_pin -pin_name soln13 -layer met9 -location {1320 840} -pin_size {1 1}
+place_pin -pin_name soln13 -layer met9 -location {1320 840} -pin_size {1 1}
 #place_pin -pin_name soln14 -layer met9 -location {1410 840} -pin_size {1 1}
 #place_pin -pin_name soln15 -layer met9 -location {1500 840} -pin_size {1 1}
 #place_pin -pin_name soln16 -layer met9 -location {1590 840} -pin_size {1 1}
@@ -40,6 +40,6 @@ place_pin -pin_name out2 -layer met9 -location {1050 660} -pin_size {1 1}
 place_pin -pin_name out3 -layer met9 -location {1140 660} -pin_size {1 1}
 place_pin -pin_name out4 -layer met9 -location {1230 660} -pin_size {1 1}
 place_pin -pin_name out5 -layer met9 -location {1320 660} -pin_size {1 1}
-#place_pin -pin_name out6 -layer met9 -location {1410 660} -pin_size {1 1}
+place_pin -pin_name out6 -layer met9 -location {1410 660} -pin_size {1 1}
 #place_pin -pin_name out7 -layer met9 -location {1500 660} -pin_size {1 1}
 #place_pin -pin_name out8 -layer met9 -location {1590 660} -pin_size {1 1}
