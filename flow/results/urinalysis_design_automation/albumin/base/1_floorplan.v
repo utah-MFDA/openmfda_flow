cc5dfc5deeafc39367e3f03f182d64f8ef5cfd1a
Erica Francis <ferica963@gmail.com>
2026-05-29 11:47:01 -0600

    Urinalysis automation results

---
+++ flow/results/urinalysis_design_automation/albumin/base/1_floorplan.v
@@ -0,0 +1,36 @@
+module albumin (out,
+    soln1,
+    soln2,
+    soln3,
+    soln4);
+ output out;
+ input soln1;
+ input soln2;
+ input soln3;
+ input soln4;
+
+ wire connect0;
+ wire connect1;
+ wire connect2;
+ wire connect3;
+ wire connect4;
+ wire connect5;
+
+ diffmix_25px_0 mix0 (.a_fluid(connect0),
+    .b_fluid(connect1),
+    .out_fluid(connect4));
+ diffmix_25px_0 mix1 (.a_fluid(connect2),
+    .b_fluid(connect4),
+    .out_fluid(connect5));
+ diffmix_25px_0 mix2 (.a_fluid(connect3),
+    .b_fluid(connect5),
+    .out_fluid(out));
+ p_serpentine_0_360_30_48 serp0 (.in_fluid(soln1),
+    .out_fluid(connect0));
+ p_serpentine_0_360_30_32 serp1 (.in_fluid(soln2),
+    .out_fluid(connect1));
+ p_serpentine_0_360_30_8 serp2 (.in_fluid(soln3),
+    .out_fluid(connect2));
+ p_serpentine_0_60_30_8 serp3 (.in_fluid(soln4),
+    .out_fluid(connect3));
+endmodule
---
+++ flow/results/urinalysis_design_automation/ethyl/base/4_final.v
@@ -0,0 +1,27 @@
+module ethyl (out,
+    soln1,
+    soln2,
+    soln3);
+ output out;
+ input soln1;
+ input soln2;
+ input soln3;
+
+ wire connect0;
+ wire connect1;
+ wire connect2;
+ wire connect3;
+
+ diffmix_25px_0 mix0 (.a_fluid(connect0),
+    .b_fluid(connect1),
+    .out_fluid(connect3));
+ diffmix_25px_0 mix1 (.a_fluid(connect2),
+    .b_fluid(connect3),
+    .out_fluid(out));
+ p_serpentine_0_360_30_81 serp0 (.in_fluid(soln1),
+    .out_fluid(connect0));
+ p_serpentine_0_60_30_1 serp1 (.in_fluid(soln2),
+    .out_fluid(connect1));
+ p_serpentine_0_30_30_1 serp2 (.in_fluid(soln3),
+    .out_fluid(connect2));
+endmodule
